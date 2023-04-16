9cde55c8fb0d78f6dabae81ff24b930cdf07ff10
devmanager987 <devmanager987@gmail.com>
2023-04-16 12:27:54 -0300

    change system title

--- pages/auth/error/index.tsx
+++ pages/auth/error/index.tsx
@@ -12,7 +12,7 @@ const ErrorPage: Page = () => {
     return (
         <div className="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
             <div className="flex flex-column align-items-center justify-content-center">
-                <img src="/demo/images/error/logo-error.svg" alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" />
+                <img src="/demo/images/error/logo-error.svg" alt="Manager logo" className="mb-5 w-6rem flex-shrink-0" />
                 <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, rgba(233, 30, 99, 0.4) 10%, rgba(33, 150, 243, 0) 30%)' }}>
                     <div className="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center" style={{ borderRadius: '53px' }}>
                         <div className="flex justify-content-center align-items-center bg-pink-500 border-circle" style={{ height: '3.2rem', width: '3.2rem' }}>
