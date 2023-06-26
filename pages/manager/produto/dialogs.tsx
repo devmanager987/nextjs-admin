70405fb6d5056ebba4ba6fdb99ad9f96fec856a8
devmanager987 <devmanager987@gmail.com>
2023-06-26 14:55:39 -0300

    Cadastro de produtos versão 1

---
+++ pages/manager/produto/dialogs.tsx
@@ -0,0 +1,112 @@
+import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
+import { classNames } from 'primereact/utils';
+
+import { Demo } from '../../../types/demo';
+
+interface ProdutoEditarProps {
+    product: Demo.Product
+    productDialog: boolean
+    submitted: boolean
+    productDialogFooter: JSX.Element
+    hideDialog: () => void
+    onCategoryChange: any
+    onInputChange: any
+    onInputNumberChange: any
+}
+
+const ProdutoEditar = ({ product, productDialog, submitted, productDialogFooter,
+    hideDialog, onCategoryChange, onInputChange, onInputNumberChange }: ProdutoEditarProps) => {
+    return (
+        <Dialog visible={productDialog} style={{ width: '450px' }} header="Product Details" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+            {product.image && <img src={`/demo/images/product/${product.image}`} alt={product.image} width="150" className="mt-0 mx-auto mb-5 block shadow-2" />}
+            <div className="field">
+                <label htmlFor="name">Nome</label>
+                <InputText id="name" value={product.name} onChange={(e) => onInputChange(e, 'name')} required autoFocus className={classNames({ 'p-invalid': submitted && !product.name })} />
+                {submitted && !product.name && <small className="p-invalid">O nome é obrigatório.</small>}
+            </div>
+            <div className="field">
+                <label htmlFor="description">Descrição</label>
+                <InputTextarea id="description" value={product.description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
+            </div>
+
+            <div className="field">
+                <label className="mb-3">Category</label>
+                <div className="formgrid grid">
+                    <div className="field-radiobutton col-6">
+                        <RadioButton inputId="category1" name="category" value="Accessories" onChange={onCategoryChange} checked={product.category === 'Accessories'} />
+                        <label htmlFor="category1">Accessórios</label>
+                    </div>
+                    <div className="field-radiobutton col-6">
+                        <RadioButton inputId="category2" name="category" value="Clothing" onChange={onCategoryChange} checked={product.category === 'Clothing'} />
+                        <label htmlFor="category2">Vestuário</label>
+                    </div>
+                    <div className="field-radiobutton col-6">
+                        <RadioButton inputId="category3" name="category" value="Electronics" onChange={onCategoryChange} checked={product.category === 'Electronics'} />
+                        <label htmlFor="category3">Eletrônicos</label>
+                    </div>
+                    <div className="field-radiobutton col-6">
+                        <RadioButton inputId="category4" name="category" value="Fitness" onChange={onCategoryChange} checked={product.category === 'Fitness'} />
+                        <label htmlFor="category4">Moda Fitness</label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="formgrid grid">
+                <div className="field col">
+                    <label htmlFor="price">Preço</label>
+                    <InputNumber id="price" value={product.price} onValueChange={(e) => onInputNumberChange(e, 'price')} mode="currency" currency="USD" locale="en-US" />
+                </div>
+                <div className="field col">
+                    <label htmlFor="quantity">Quantidade</label>
+                    <InputNumber id="quantity" value={product.quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} />
+                </div>
+            </div>
+        </Dialog>
+    )
+}
+
+interface ProdutoExcluirProps {
+    product: Demo.Product
+    deleteProductDialog: boolean
+    deleteProductDialogFooter: JSX.Element
+    hideDeleteProductDialog: () => void
+}
+
+const ProdutoExcluir = ({ product, deleteProductDialog, deleteProductDialogFooter, hideDeleteProductDialog }: ProdutoExcluirProps) => {
+    return (
+        <Dialog visible={deleteProductDialog} style={{ width: '450px' }} header="Confirm" modal footer={deleteProductDialogFooter} onHide={hideDeleteProductDialog}>
+            <div className="flex align-items-center justify-content-center">
+                <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                {product && (
+                    <span>
+                        Tem certeza de que deseja excluir <b>{product.name}</b>?
+                    </span>
+                )}
+            </div>
+        </Dialog>
+    )
+}
+
+interface ProdutoExcluirVariosProps {
+    product: Demo.Product
+    deleteProductsDialog: boolean
+    deleteProductsDialogFooter: JSX.Element
+    hideDeleteProductsDialog: () => void
+}
+
+const ProdutoExcluirVarios = ({ product, deleteProductsDialog, deleteProductsDialogFooter, hideDeleteProductsDialog }: ProdutoExcluirVariosProps) => {
+    return (
+        <Dialog visible={deleteProductsDialog} style={{ width: '450px' }} header="Confirm" modal footer={deleteProductsDialogFooter} onHide={hideDeleteProductsDialog}>
+            <div className="flex align-items-center justify-content-center">
+                <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                {product && <span>Tem certeza de que deseja excluir os produtos selecionados?</span>}
+            </div>
+        </Dialog>
+    )
+}
+
+export { ProdutoEditar, ProdutoExcluir, ProdutoExcluirVarios }
