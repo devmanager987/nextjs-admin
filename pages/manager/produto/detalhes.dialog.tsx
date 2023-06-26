4f9444210177686352d3356c0e511bd38d502dcc
devmanager987 <devmanager987@gmail.com>
2023-06-26 15:52:09 -0300

    Cadastro de Produtos versão 1.1

--- pages/manager/produto/detalhes.dialog.tsx
+++ pages/manager/produto/detalhes.dialog.tsx
@@ -22,34 +22,34 @@ const ProdutoEditar = ({ product, productDialog, submitted, productDialogFooter,
     hideDialog, onCategoryChange, onInputChange, onInputNumberChange }: ProdutoEditarProps) => {
     return (
         <Dialog visible={productDialog} style={{ width: '450px' }} header="Product Details" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
-            {product.image && <img src={`/demo/images/product/${product.image}`} alt={product.image} width="150" className="mt-0 mx-auto mb-5 block shadow-2" />}
+            {product?.image && <img src={`/demo/images/product/${product?.image}`} alt={product?.image} width="150" className="mt-0 mx-auto mb-5 block shadow-2" />}
             <div className="field">
                 <label htmlFor="name">Nome</label>
-                <InputText id="name" value={product.name} onChange={(e) => onInputChange(e, 'name')} required autoFocus className={classNames({ 'p-invalid': submitted && !product.name })} />
-                {submitted && !product.name && <small className="p-invalid">O nome é obrigatório.</small>}
+                <InputText id="name" value={product?.name} onChange={(e) => onInputChange(e, 'name')} required autoFocus className={classNames({ 'p-invalid': submitted && !product?.name })} />
+                {submitted && !product?.name && <small className="p-invalid">O nome é obrigatório.</small>}
             </div>
             <div className="field">
                 <label htmlFor="description">Descrição</label>
-                <InputTextarea id="description" value={product.description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
+                <InputTextarea id="description" value={product?.description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
             </div>
 
             <div className="field">
                 <label className="mb-3">Category</label>
                 <div className="formgrid grid">
                     <div className="field-radiobutton col-6">
-                        <RadioButton inputId="category1" name="category" value="Accessories" onChange={onCategoryChange} checked={product.category === 'Accessories'} />
+                        <RadioButton inputId="category1" name="category" value="Accessories" onChange={onCategoryChange} checked={product?.category === 'Accessories'} />
                         <label htmlFor="category1">Accessórios</label>
                     </div>
                     <div className="field-radiobutton col-6">
-                        <RadioButton inputId="category2" name="category" value="Clothing" onChange={onCategoryChange} checked={product.category === 'Clothing'} />
+                        <RadioButton inputId="category2" name="category" value="Clothing" onChange={onCategoryChange} checked={product?.category === 'Clothing'} />
                         <label htmlFor="category2">Vestuário</label>
                     </div>
                     <div className="field-radiobutton col-6">
-                        <RadioButton inputId="category3" name="category" value="Electronics" onChange={onCategoryChange} checked={product.category === 'Electronics'} />
+                        <RadioButton inputId="category3" name="category" value="Electronics" onChange={onCategoryChange} checked={product?.category === 'Electronics'} />
                         <label htmlFor="category3">Eletrônicos</label>
                     </div>
                     <div className="field-radiobutton col-6">
-                        <RadioButton inputId="category4" name="category" value="Fitness" onChange={onCategoryChange} checked={product.category === 'Fitness'} />
+                        <RadioButton inputId="category4" name="category" value="Fitness" onChange={onCategoryChange} checked={product?.category === 'Fitness'} />
                         <label htmlFor="category4">Moda Fitness</label>
                     </div>
                 </div>
@@ -58,11 +58,11 @@ const ProdutoEditar = ({ product, productDialog, submitted, productDialogFooter,
             <div className="formgrid grid">
                 <div className="field col">
                     <label htmlFor="price">Preço</label>
-                    <InputNumber id="price" value={product.price} onValueChange={(e) => onInputNumberChange(e, 'price')} mode="currency" currency="USD" locale="en-US" />
+                    <InputNumber id="price" value={product?.price} onValueChange={(e) => onInputNumberChange(e, 'price')} mode="currency" currency="USD" locale="en-US" />
                 </div>
                 <div className="field col">
                     <label htmlFor="quantity">Quantidade</label>
-                    <InputNumber id="quantity" value={product.quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} />
+                    <InputNumber id="quantity" value={product?.quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} />
                 </div>
             </div>
         </Dialog>
--- pages/manager/produto/index.tsx
+++ pages/manager/produto/index.tsx
@@ -3,16 +3,13 @@
 import { Button } from 'primereact/button';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
-import { Dialog } from 'primereact/dialog';
 import { FileUpload } from 'primereact/fileupload';
-import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber';
+import { InputNumberValueChangeEvent } from 'primereact/inputnumber';
 import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
+import { RadioButtonChangeEvent } from 'primereact/radiobutton';
 import { Rating } from 'primereact/rating';
 import { Toast } from 'primereact/toast';
 import { Toolbar } from 'primereact/toolbar';
-import { classNames } from 'primereact/utils';
 import React, { useEffect, useRef, useState } from 'react';
 import { ProductService } from '../../../demo/service/ProductService';
 import { Demo } from '../../../types/types';
